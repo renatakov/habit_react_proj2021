3da0959b6e003e439ca6e585a1f15faea5dd70d8
renatakov <renatakov1010@gmail.com>
2021-06-18 15:25:50 +0300

    888

--- src/pages/HabitItemPage/Line.js
+++ src/pages/HabitItemPage/Line.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 
 const data = {
   labels: ['19.06', '20.06', '22.06', '23.06', '25.06', '26.06'],
   datasets: [
     {
-
       data: [12, 19, 3, 5, 2, 3],
       fill: false,
       backgroundColor: 'rgb(173, 129, 209)',
@@ -26,13 +25,15 @@ const options = {
   },
 };
 
-const LineChart = () => (
+const LineChart = () => {
+  return(
   <>
-    <div className='header'>
-      <h1 className='title'>Диаграмма привычки</h1>
+    <div className="header">
+      <h1 className="title">Диаграмма привычки</h1>
     </div>
     <Line data={data} options={options} />
   </>
-);
+  );
+};
 
 export default LineChart;
